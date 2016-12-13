01573f2f930d86820f7a913844a78694ea95130b
Philipp Schmitt <philipp@schmitt.co>
2016-12-13 09:06:37 +0100

    Support SSL connections

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM homeassistant/home-assistant:latest
 MAINTAINER Philipp Schmitt <philipp@schmitt.co>
 
-ENV HASS_HOSTNAME=example.com HASS_PORT=8123 HASS_PASSWORD=password
+ENV HASS_HOSTNAME=example.com \
+    HASS_PORT=8123 \
+    HASS_PASSWORD=password \
+    HASS_USE_SSL=False
+
 EXPOSE 8123
 COPY slave.py /usr/src/app/slave.py
 
--- slave.py
+++ slave.py
@@ -14,10 +14,16 @@ import os
 HASS_HOSTNAME = os.environ.get('HASS_HOSTNAME', 'example.com')
 HASS_PORT = os.environ.get('HASS_PORT', 8123)
 HASS_PASSWORD = os.environ.get('HASS_PASSWORD', 'password')
+HASS_USE_SSL = bool(os.environ.get('HASS_USE_SSL', False))
 
 # Location of the Master API: host, password, port.
 # Password and port are optional.
-remote_api = remote.API(HASS_HOSTNAME, HASS_PASSWORD, HASS_PORT)
+remote_api = remote.API(
+    host=HASS_HOSTNAME,
+    api_password=HASS_PASSWORD,
+    port=HASS_PORT,
+    use_ssl=HASS_USE_SSL
+)
 
 # Initialize slave
 hass = remote.HomeAssistant(remote_api)
